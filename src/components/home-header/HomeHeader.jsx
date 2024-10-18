0593cbb9d8c2e68812dfad39d3803190bbef6acb
Wajdi Aweida <wajdi.aweida@gmail.com>
2024-10-18 21:16:03 +0300

    adding home-page heade

--- src/components/home-header/HomeHeader.jsx
+++ src/components/home-header/HomeHeader.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
 const HomeHeader = () => {
     return (
-        <div>
-            Hello, World!
-        </div>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Crystask
+                </Typography>
+                <Button color="inherit">Profile</Button>
+            </Toolbar>
+        </AppBar>
     )
 }
 
