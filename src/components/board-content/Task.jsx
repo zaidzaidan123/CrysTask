b4d06a1f9e4770d09f1dbe16335f1d40064b0d32
wajdiaw <wajdi.aweida@gmail.com>
2024-10-24 13:47:53 +0300

    task component changed, go ahead button added

--- src/components/board-content/Task.jsx
+++ src/components/board-content/Task.jsx
@@ -1,23 +1,29 @@
-import {Button, Card} from "@mui/material";
+import {Button, Card, Stack} from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import React from "react";
-
+import {TrendingFlat} from "@mui/icons-material";
+import "./styles.css";
 
 const Task = ({title, description, onClick}) => {
     return (
-        <Button sx={{ padding: 0, textAlign: "left" }} onClick={onClick}>
-            <Card sx={{ width: "100%" }}>
-                <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">
-                        {title}
-                    </Typography>
-                    <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                        {description}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </Button>
+
+        <Card sx={{ width: "100%" }}>
+            <CardContent>
+                <Typography gutterBottom variant="h6" component="div">
+                    {title}
+                </Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                    {description}
+                </Typography>
+            </CardContent>
+            <Button className="move-ahead-button"  onClick={onClick}>
+                <Stack direction={"row"} alignItems={"center"}>
+                    <Typography>Move Ahead</Typography>
+                    <TrendingFlat fontSize="small" />
+                </Stack>
+            </Button>
+        </Card>
+
     );
 };
 
